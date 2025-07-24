8741ea4c67ecf1e5d8309d4144a8994ebdf11a77
gyiptgyipt <htetwaiyan.zyme@gmail.com>
2025-07-24 17:47:20 +0630

    icon&&about

--- src/components/navigation/NavButton.jsx
+++ src/components/navigation/NavButton.jsx
@@ -27,8 +27,8 @@ const getIcon = (icon) => {
       return <Phone className="w-full h-auto" strokeWidth={1.5} />;
     case "github":
       return <Github className="w-full h-auto" strokeWidth={1.5} />;
-    case "linkedin":
-      return <Linkedin className="w-full h-auto" strokeWidth={1.5} />;
+    case "youtube":
+      return <Youtube className="w-full h-auto" strokeWidth={1.5} />;
     case "facebook":
       return <Facebook className="w-full h-auto" strokeWidth={1.5} />;
     case "resume":
