724c1edacdbbcd931175c2198f075ce709b5f9ae
gyiptgyipt <htetwaiyan.zyme@gmail.com>
2025-12-30 00:20:16 +0630

    prj_images

--- src/app/data.js
+++ src/app/data.js
@@ -17,9 +17,27 @@ Websites:
 */
 
 export const projectsData = [
-  
+
   {
     id: 1,
+    name: "Myanmar's First Service Robot",
+    description: "The Myanmar's first differential drive robot for Service",
+    date: "2025-10-14",
+    demoLink: "https://www.facebook.com/share/v/1AcCzwBtgq/",
+    image: "/project_images/kilo.jpeg",
+  },
+
+  {
+    id: 2,
+    name: "Multi Robot APP",
+    description: "QT based Multi Robot Control Software",
+    date: "2025-10-23",
+    demoLink: "https://www.facebook.com/reel/1379220050881589",
+    image: "/project_images/multi_robot_app.jpeg",
+  },
+  
+  {
+    id: 3,
     name: "BEE BOT (ROM2109)",
     description: "Differential Drive Robot for Educational Prototype",
     date: "2023-06-15",
@@ -27,82 +45,95 @@ export const projectsData = [
     image: "/project_images/beebot.jpeg",
   },
   {
-    id: 2,
+    id: 4,
     name: "LOC_DIFF",
     description: "Robot Localization using Differential Drive for outdoor navigation",
     date: "2023-06-22",
     demoLink: "https://github.com/gyiptgyipt/loc_diff",
     image: "/project_images/gps_waypoints.png",
   },
+
   {
-    id: 3,
+    id: 5,
+    name: "PX4_SITL UAVs show",
+    description: "On going project (uavs show)",
+    date: "2025-7-15",
+    demoLink: "https://www.linkedin.com/feed/update/urn:li:activity:7410572787662270464/",
+    image: "/project_images/UAV_show.jpg",
+  },
+
+  {
+    id: 6,
     name: "UltraSonic Mapping",
     description: "HCSR04 with MPU6050 using micro_ros and SLAM_toolbox for mapping",
     date: "2023-09-10",
     demoLink: "https://github.com/gyiptgyipt/sonic_to_laser",
+    image: "/project_images/ultrasonics_mapping.jpg",
   },
   {
-    id: 4,
+    id: 7,
     name: "FUN_VIZ ",
     description: "Web based visualization for ros2",
     date: "2024-10-20",
     demoLink: "https://github.com/gyiptgyipt/funviz",
+    image: "/project_images/funviz.jpeg",
   },
   {
-    id: 5,
+    id: 8,
     name: "Scalar Punk",
     description: " Robot emotion animation using QT widget and ros2",
     date: "2025-04-12",
     demoLink: "https://github.com/gyiptgyipt/ScalarPunk",
+    image: "/project_images/emotions.png",
   },
   {
-    id: 6,
+    id: 9,
     name: "Navigation2 Documentation",
     description: "ROS2 based Navigation2 documentation for robot navigation in Burmese Language",
     date: "2024-6-24",
     demoLink: "https://www.facebook.com/share/p/16pqy8xrkz/",
+    image: "/project_images/contribution_in_romdynamics.jpeg",
   },
+  
   {
-    id: 7,
-    name: "BOBO Service Robot",
-    description: "The Myanmar's first differential drive robot for Service",
-    date: "2022-04-18",
-    demoLink: "https://studybuddy.example.com",
-  },
-  {
-    id: 8,
+    id: 10,
     name: "ROM_DYNAMICS Mission Controller APP",
     description: "QT based Mission Controller App for ROS2",
     date: "2024-9-5",
-    demoLink: "https://romdynamics.com/",
+    demoLink: "https://www.facebook.com/100004448652681/videos/a.2194835780674689/1074629197842443",
   },
+  
   {
-    id: 9,
-    name: "PX4_SITL UAVs show",
-    description: "On going project (uavs show)",
-    date: "2025",
-    demoLink: "https://github.com/gyiptgyipt/multi_uavs_swarm",
-  },
-  {
-    id: 10,
+    id: 11,
     name: "F110(F1Tenth Tutorials)",
     description: "F1tenth GYM",
     date: "2024-02-14",
     demoLink: "https://github.com/gyiptgyipt/f1tenth",
   },
   {
-    id: 11,
+    id: 12,
     name: "Black Donut Simulation",
     description: "Custom robot simulation in gazebo",
     date: "2023-08-27",
     demoLink: "https://github.com/stallonezwel/black_donut_simulation",
+    image: "/project_images/black_donut.jpeg",
   },
   {
-    id: 12,
-    name: "Speaker in webinar",
+    id: 13,
+    name: "Speaker in online webinar",
     description: "Public speaking with topic name 'AI across Robotics' in UNI Learn AI Webinar",
     date: "2025-09-12",
     demoLink: "https://www.facebook.com/photo?fbid=122163284732478231&set=pcb.122163284804478231",
+    image: "/project_images/face_pto.jpeg",
+  }
+
+  {
+    id: 13,
+    name: "Speaker in IT expo in YCC",
+    description: "Public speaking with topic name 'AMR For Industrial Revolution' in YCC(Yangon)",
+    date: "2025-09-12",
+    demoLink: "https://www.youtube.com/watch?v=Qf9ZzsfRt44",
+    image: "/project_images/channel_pic.jpeg",
   }
 ];
 
